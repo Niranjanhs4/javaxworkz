0c870c3fc3e031c76dac41a7c9db472df5a5981e
Niranjanhs4 <niranjanxworkz@gmail.com>
2022-06-08 20:20:32 +0530

    java_pgm

---
+++ ifelseexample.java
@@ -0,0 +1,12 @@
+class ifelseexample{
+public static void main(String[] args){
+//boolean exampass=true;
+boolean exampass=!true;
+if(exampass){
+System.out.println("niranjan clear the exam");
+}
+else{
+System.out.println("niranjan fail in exam");
+}
+}
+ }
